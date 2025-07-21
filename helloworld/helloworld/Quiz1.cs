86c251e54bfc4cada54c3861f92caca1265356d2
Uppaladharani <20551a4252.dharani@gmail.com>
2025-07-21 21:51:23 +0530

    first commit

---
+++ helloworld/helloworld/Quiz1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    internal class Quiz1
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c =a+b;
+            Console.WriteLine(c);
+        }
+    }
+}
